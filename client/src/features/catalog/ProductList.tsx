47eed19a079e0ecb6879a09850c5b1f5b7c7dcc7
ddupree2001 <devondupree01@gmail.com>
2024-06-26 19:52:46 -0700

    end of section 8

--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -1,17 +1,24 @@
 import { Product } from "../../app/models/product";
 import { Grid } from "@mui/material";
 import ProductCard from "./ProductCard";
+import { useAppSelector } from "../../app/store/configureStore";
+import ProductCardSkeleton from "./ProductCardSkeleton";
 
 interface Props {
   products: Product[];
 }
 
 export default function ProductList({ products }: Props) {
+  const { productsLoaded } = useAppSelector((state) => state.catalog);
   return (
     <Grid container spacing={4}>
       {products.map((product) => (
-          <Grid item xs={12} sm={6} md={3} key={product.id}>
-          <ProductCard product={product} />
+        <Grid item xs={12} sm={6} md={4} key={product.id}>
+          {!productsLoaded ? (
+            <ProductCardSkeleton />
+          ) : (
+            <ProductCard product={product} />
+          )}
         </Grid>
       ))}
     </Grid>
